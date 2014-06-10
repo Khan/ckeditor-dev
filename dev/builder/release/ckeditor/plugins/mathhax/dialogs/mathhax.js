c311bb258bd8c280548eb7d2d7c429f4c262c2d8
Joel Burget <joel@khanacademy.org>
2014-06-10 15:33:36 -0700

    Add mathhax!

---
+++ dev/builder/release/ckeditor/plugins/mathhax/dialogs/mathhax.js
@@ -0,0 +1,7 @@
+﻿/*
+ Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+var TeX=KAdefine.require("react-components/tex.jsx");
+CKEDITOR.dialog.add("mathhax",function(){var b;return{title:"LaTeX",minWidth:350,minHeight:100,contents:[{id:"info",elements:[{id:"equation",type:"textarea",onLoad:function(){this.getInputElement().on("keyup",function(){var a=this.getInputElement().getValue();b(a)}.bind(this))},setup:function(a){this.setValue(a.data.math)},commit:function(a){a.setData("math",this.getValue())}},{id:"preview",type:"html",html:'<code class="latex" style="font-size: 150%;"></code>',onLoad:function(){b=function(a){var b=
+CKEDITOR.document.getById(this.domId).$;React.renderComponent(TeX({},a),b)}.bind(this);b("")},setup:function(a){b(a.data.math)}}]}]}});
